e9efd69df0245c192deac3372ccf51d8c99e5640
Adarshmishra2002 <adarshmishra1042002@gmail.com>
2025-01-16 00:04:53 +0530

    node project with dockerfile

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+const path = __dirname + '/views/';
+const port = 8080;
+
+// Middleware to log requests
+router.use(function (req, res, next) {
+  console.log('/' + req.method);
+  next();
+});
+
+// Routes
+router.get('/', function(req, res) {
+  res.sendFile(path + 'index.html');
+});
+
+router.get('/sharks', function(req, res) {
+  res.sendFile(path + 'sharks.html');
+});
+
+
+// Serve static files
+app.use(express.static(path));
+
+// Use the router for routing
+app.use('/', router);
+
+// Start the server
+app.listen(port, function () {
+  console.log('Example app listening on port 8080!');
+});
