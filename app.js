1d44a7ed3fa2199861177b0c41421a97fe6cc127
Adarsh <adarshmishra1042002@gmail.com>
2025-01-23 00:32:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const router = express.Router();
 
 const path = __dirname + '/views/';
-const port = 8080;
+const port = 8081;
 
 // Middleware to log requests
 router.use(function (req, res, next) {
@@ -29,5 +29,5 @@ app.use('/', router);
 
 // Start the server
 app.listen(port, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('Example app listening on port 8081!');
 });
